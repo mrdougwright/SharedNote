8415dcbabdd60b35d9800fc22d73d6674854cc71
Doug Wright <mrdougwright@gmail.com>
2021-03-15 22:58:34 -0500

    Initial Commit

---
+++ SharedNote/SharedNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedNoteApp.swift
+//  SharedNote
+//
+//  Created by Doug Wright on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SharedNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
