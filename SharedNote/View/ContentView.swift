fb8863f4e5afec0b1730eff27f530822bc737d06
Doug Wright <mrdougwright@gmail.com>
2021-03-22 11:04:53 -0500

    remove old api key 😅

--- SharedNote/View/ContentView.swift
+++ SharedNote/View/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var selectedTab = 0
     @State private var tagNumber: Int = 0
     @ObservedObject var viewModel = NotesViewModel()
 
